c28eeb4c890da73b3c50e9e4ec82356c6cac7753
Alizhan Manap <254732@astanait.edu.kz>
2025-12-26 09:22:15 +0500

    that is my main

--- src/Addition.java
+++ src/Addition.java
@@ -11,6 +11,11 @@ public class Addition {
         System.out.print("Enter first integer: ");
         int number1 = input.nextInt();
 
-        System.out.print("Alizhan Krutoi");
+        System.out.print("Enter second integer: ");
+        int number2 = input.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.printf("Sum is %d%n", sum);
     }
 }
