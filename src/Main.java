405d6fb9e6ff32d874e464e5d488f6493fcc8bf5
Alizhan Manap <254732@astanait.edu.kz>
2025-12-26 09:10:51 +0500

    that is my main

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+import java.util.Scanner;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int number1 = input.nextInt();
+
+        System.out.print("");
+    }
+}
